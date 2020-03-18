259eebb7977f440973c6da0f73d2fcc1c6fdc5ab
Alex Filatov <tanyaarni@gmail.com>
2020-03-18 20:23:36 +0200

    Filatov Oleksandr lab_3

--- core.clj
+++ core.clj
@@ -5,25 +5,25 @@
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]))
 
-(defn readTSV [name]                                        ;парсит tsv file
+(defn readTSV [name]
   (map #(str/split % #"\t")
        (str/split (slurp name) #"\r\n")))
 
-(defn readCSV [name]                                        ; парсит csv filr
+(defn readCSV [name]
   (with-open [reader (io/reader name)]
     (doall
       (csv/read-csv reader))))
 
-(defn data->maps [head & lines]                             ;крнвертирует данніе в колекцію map ключ значение
+(defn data->maps [head & lines]
   (map #(zipmap (map keyword head) %1) lines))
 
-(defn makeTableTSV [name]                                   ;создает табличку с tsv файла
+(defn makeTableTSV [name]
   (apply data->maps (readTSV name)))
 
-(defn makeTableCSV [name]                                   ;создает табличку с csv файла
+(defn makeTableCSV [name]
   (apply data->maps (readCSV name)))
 
-(defn printTable [table]                                    ;вивод таблички в консоль
+(defn printTable [table]
   (loop [k 0]
     (when (< k (count (first table)))
       (print (format "%40s| " (name (nth (keys (first table)) k))))
@@ -38,14 +38,14 @@
       (println "")
       (recur (+ i 1)))))
 
-(defn checkFormat [name]                                    ;прроверка формата файла
+(defn checkFormat [name]
   (def formatFile (str/split name #"\."))
   (if (= (str (nth formatFile 1)) "csv") true false))
 
 (defn -main
   [& args]
   (println "Input name of file: ")
-  (def input (read-line))                                   ;запрос на ввод строки
+  (def input (read-line))
   ;(def input "mp-assistants.csv")
   (if (checkFormat input)
     (printTable (makeTableCSV input))
