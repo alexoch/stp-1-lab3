d3cdf8ecf758ea8efb751dd2271d58b9f47a26c6
Vlad gribovskiy <45690086+Trafi25@users.noreply.github.com>
2020-03-31 15:54:48 +0300

    Add files via upload

--- main.lisp
+++ main.lisp
@@ -14,25 +14,25 @@
 	(space_str_dop (subseq string 0 n) separator (cons (subseq string (1+ n)) r))
       (cons string r)))) 
 
-;;; Read file into list by lines
+
 (defun read_file_in_line (OurInput &optional (TableData '()))
 	(setq line (read-line OurInput nil))
 	(cond
 		(line (read_file_in_line OurInput (append TableData (list line))))
 		(t TableData))) 
 
-;;; Parse table
+
 (defun recognize_file (TableData)
 	(cond
 		((expansion OurPath "tsv") (recognize_file_separator TableData "|"))
 		((expansion OurPath "csv") (recognize_file_separator TableData ","))
 		(t (error "Not correct file extension. Pls correct the mistake. File path: ~S" OurPath))))
 
-;;; Parse table with passed separator
+
 (defun recognize_file_separator (TableData separator)
 	(mapcar #'(lambda (row) (space_str row separator)) TableData))
 
-;;; Pretty table output to stdout
+
 (defun print_table (table)
 	(mapcar
 		#'(lambda (row)
@@ -44,7 +44,6 @@
 		table)) 
 
 
-;;; Tables pathes
 
 (setq table_path "./")
 (import 'charset:utf-8 'keyword)
@@ -54,7 +53,7 @@
 	(setq index (search "(" query))
 	(cond
 		((string= (substring query 0 index) "load") "load")
-		((string= (substring query 0 index) "exit") "exit")		
+		((string= (substring query 0 index) "exit") "exit")	))	
 
 
 (defun command_row (query)
@@ -64,26 +63,25 @@
 		param (substring query paramStartIndex paramEndIndex))
 	param)
 
-;;; Function that executes a command
+
 (defun command_execution (command query)
 	(cond
 		((string= command "load")
 			(setq
-				OurName (command_row query)
-				OurPath (concatenate 'string table_path OurName)
-				OurInput (open OurPath :external-format :utf-8)
-				TableData (read_file_in_line OurInput)
+				OurName (command_row query)				
+				TableData (read_file_in_line (open OurName :external-format :utf-8))
 				table (recognize_file TableData))
 			(print_table table))
 		((string= command "exit") (exit))
 		(t (error "Command is not correct: ~S" query)))) 
 
 
-	;; Read table index
-	(setq query (read-line))
-	(clear-input)
-	;; Parse user command
+
+
+	;; start
+	(setq query (read-line))	
+	
 	(setq command (command_analysis query))
-	;; Execute user's command
+	
 	(command_execution command query))
 
